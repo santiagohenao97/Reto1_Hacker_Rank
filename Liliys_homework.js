7f79fa04f602b7ed3a33787c37c99eacdad89cc3
santiagohenao97 <santiago.henao@hotmail.com>
2019-08-26 22:58:53 -0500

    no es mucho pero es trabajo honesto

--- Liliys_homework.js
+++ Liliys_homework.js
@@ -1,4 +1,5 @@
-let arr=[2,5,3,1,4,6]
+let arr=[5400, 3816, 5154, 2940, 5760, 7938, 6831, 6750, 9240]
+let arr2= arr.slice()
 
 let arrCopy= arr.slice()
 
@@ -6,13 +7,12 @@ arrCopy=arrCopy.sort(function (num1, num2) {
     return num1 - num2;
 });
 
+let arrRev= arrCopy.slice().reverse()
 
+console.log(arrCopy)
 
+let acc=0, acc2=0;
 
-let acc=0;
-
-
- 
    for(let i=0; i<arrCopy.length;i++){
         if(arr[i]!==arrCopy[i]){
         
@@ -25,10 +25,26 @@ let acc=0;
             //arr.unshift(eliminar)
             console.log("Agregué "+ eliminar+ " mi nuevo arr es " + arr )
             acc++
-        }  
+        }
+
+       
+        if(arr2[i]!==arrRev[i]){
+        
+            let donde=arr2.indexOf(arrRev[i])
+            console.log("2. encontré " + arrRev[i] + " en la posición " + donde)
+            let eliminar=arr2.splice(donde,1)
+            console.log("2. eliminé " + eliminar )
+            console.log("2. mi nuevo arr es " + arr2 )
+            arr2.splice(i,0,eliminar)
+            //arr2.unshift(eliminar)
+            console.log("2. Agregué "+ eliminar+ " mi nuevo arr es " + arr2 )
+            acc2++
+        }
+        
    }
 
-   console.log(acc)
+   console.log(acc + "y" + acc2)
+   console.log(Math.min(acc,acc2));
 
    
 
